ff69c76cacc49446566594a481ce6106f158023c
admon fixer <cscbarranquilla@fixer.repair>
2025-11-29 05:39:27 -0500

    con

--- src/pages/Views/Dashboard/index.jsx
+++ src/pages/Views/Dashboard/index.jsx
@@ -53,7 +53,7 @@ const Dashboard = ({ trabajadores = [] }) => {
   const [videoURL, setVideoURL] = useState("");
   const [videoFile, setVideoFile] = useState(null);
 
-  const API_BASE_URL = "http://localhost:5000";
+  const API_BASE_URL = import.meta.env.VITE_API_URL // "http://localhost:5000";
 
   // ============================
   // GET videos
